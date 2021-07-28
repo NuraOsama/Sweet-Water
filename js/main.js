0de11822dde929fb53b98d0f8c96ff772efc03ef
NuraOsama <47220662+NuraOsama@users.noreply.github.com>
2021-07-29 00:05:11 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,308 @@
+(function ($) {
+
+    "use strict";
+    new WOW().init();
+
+     //aside header
+    $(".nav-toggle").click(function(){
+        $(".aside-header").addClass('active');
+        });
+        
+        
+        $(".close-header").click(function(){
+          $(".aside-header").removeClass('active');
+          
+          });
+
+           //work tabs
+          $(".first-text").addClass('active');
+
+          $(".first-text").hover(function(){
+            $(".first-image").fadeIn();
+            $(".second-image").fadeOut();
+            $(".third-image").fadeOut();
+            $(".fourth-image").fadeOut();
+            $(".fifth-image").fadeOut();
+            $(".sixth-image").fadeOut();
+            $(".seventh-image").fadeOut();
+            $(".eightn-image").fadeOut();
+            $(".ninth-image").fadeOut();
+            $(".tenth-image").fadeOut();
+            $(".eleventh-image").fadeOut();
+            $(".second-text").removeClass('active');
+            $(".third-text").removeClass('active');
+            $(".fourth-text").removeClass('active');
+            $(".fifth-text").removeClass('active');
+            $(".sixth-text").removeClass('active');
+            $(".seventh-text").removeClass('active');
+            $(".eightn-text").removeClass('active');
+            $(".ninth-text").removeClass('active');
+            $(".tenth-text").removeClass('active');
+            $(".eleventh-text").removeClass('active');
+            $(this).addClass('active');
+
+          });
+
+          $(".second-text").hover(function(){
+            $(".second-image").fadeIn();
+            $(".first-image").fadeOut();
+            $(".third-image").fadeOut();
+            $(".fourth-image").fadeOut();
+            $(".fifth-image").fadeOut();
+            $(".sixth-image").fadeOut();
+            $(".seventh-image").fadeOut();
+            $(".eightn-image").fadeOut();
+            $(".ninth-image").fadeOut();
+            $(".tenth-image").fadeOut();
+            $(".eleventh-image").fadeOut();
+            $(".first-text").removeClass('active');
+            $(".third-text").removeClass('active');
+            $(".fourth-text").removeClass('active');
+            $(".fifth-text").removeClass('active');
+            $(".sixth-text").removeClass('active');
+            $(".seventh-text").removeClass('active');
+            $(".eightn-text").removeClass('active');
+            $(".ninth-text").removeClass('active');
+            $(".tenth-text").removeClass('active');
+            $(".eleventh-text").removeClass('active');
+            $(this).addClass('active');
+
+          });
+          $(".third-text").hover(function(){
+            $(".third-image").fadeIn();
+            $(".first-image").fadeOut();
+            $(".second-image").fadeOut();
+            $(".fourth-image").fadeOut();
+            $(".fifth-image").fadeOut();
+            $(".sixth-image").fadeOut();
+            $(".seventh-image").fadeOut();
+            $(".eightn-image").fadeOut();
+            $(".ninth-image").fadeOut();
+            $(".tenth-image").fadeOut();
+            $(".eleventh-image").fadeOut();
+            $(".first-text").removeClass('active');
+            $(".second-text").removeClass('active');
+            $(".fourth-text").removeClass('active');
+            $(".fifth-text").removeClass('active');
+            $(".sixth-text").removeClass('active');
+            $(".seventh-text").removeClass('active');
+            $(".eightn-text").removeClass('active');
+            $(".ninth-text").removeClass('active');
+            $(".tenth-text").removeClass('active');
+            $(".eleventh-text").removeClass('active');
+            $(this).addClass('active');
+
+          });
+          $(".fourth-text").hover(function(){
+            $(".fourth-image").fadeIn();
+            $(".first-image").fadeOut();
+            $(".second-image").fadeOut();
+            $(".third-image").fadeOut();
+            $(".fifth-image").fadeOut();
+            $(".sixth-image").fadeOut();
+            $(".seventh-image").fadeOut();
+            $(".eightn-image").fadeOut();
+            $(".ninth-image").fadeOut();
+            $(".tenth-image").fadeOut();
+            $(".eleventh-image").fadeOut();
+            $(".first-text").removeClass('active');
+            $(".second-text").removeClass('active');
+            $(".third-text").removeClass('active');
+            $(".fifth-text").removeClass('active');
+            $(".sixth-text").removeClass('active');
+            $(".seventh-text").removeClass('active');
+            $(".eightn-text").removeClass('active');
+            $(".ninth-text").removeClass('active');
+            $(".tenth-text").removeClass('active');
+            $(".eleventh-text").removeClass('active');
+            $(this).addClass('active');
+
+          });
+
+
+          $(".fifth-text").hover(function(){
+            $(".fifth-image").fadeIn();
+            $(".first-image").fadeOut();
+            $(".second-image").fadeOut();
+            $(".third-image").fadeOut();
+            $(".fourth-image").fadeOut();
+            $(".sixth-image").fadeOut();
+            $(".seventh-image").fadeOut();
+            $(".eightn-image").fadeOut();
+            $(".ninth-image").fadeOut();
+            $(".tenth-image").fadeOut();
+            $(".eleventh-image").fadeOut();
+            $(".first-text").removeClass('active');
+            $(".second-text").removeClass('active');
+            $(".third-text").removeClass('active');
+            $(".fourth-text").removeClass('active');
+            $(".sixth-text").removeClass('active');
+            $(".seventh-text").removeClass('active');
+            $(".eightn-text").removeClass('active');
+            $(".ninth-text").removeClass('active');
+            $(".tenth-text").removeClass('active');
+            $(".eleventh-text").removeClass('active');
+            $(this).addClass('active');
+
+          });
+
+
+          $(".sixth-text").hover(function(){
+            $(".sixth-image").fadeIn();
+            $(".first-image").fadeOut();
+            $(".second-image").fadeOut();
+            $(".third-image").fadeOut();
+            $(".fourth-image").fadeOut();
+            $(".fifth-image").fadeOut();
+            $(".seventh-image").fadeOut();
+            $(".eightn-image").fadeOut();
+            $(".ninth-image").fadeOut();
+            $(".tenth-image").fadeOut();
+            $(".eleventh-image").fadeOut();
+            $(".first-text").removeClass('active');
+            $(".second-text").removeClass('active');
+            $(".third-text").removeClass('active');
+            $(".fourth-text").removeClass('active');
+            $(".fifth-text").removeClass('active');
+            $(".seventh-text").removeClass('active');
+            $(".eightn-text").removeClass('active');
+            $(".ninth-text").removeClass('active');
+            $(".tenth-text").removeClass('active');
+            $(this).addClass('active');
+
+          });
+
+
+          $(".seventh-text").hover(function(){
+            $(".seventh-image").fadeIn();
+            $(".first-image").fadeOut();
+            $(".second-image").fadeOut();
+            $(".third-image").fadeOut();
+            $(".fourth-image").fadeOut();
+            $(".fifth-image").fadeOut();
+            $(".sixth-image").fadeOut();
+            $(".eightn-image").fadeOut();
+            $(".ninth-image").fadeOut();
+            $(".tenth-image").fadeOut();
+            $(".eleventh-image").fadeOut();
+            $(".first-text").removeClass('active');
+            $(".second-text").removeClass('active');
+            $(".third-text").removeClass('active');
+            $(".fourth-text").removeClass('active');
+            $(".fifth-text").removeClass('active');
+            $(".sixth-text").removeClass('active');
+            $(".eightn-text").removeClass('active');
+            $(".ninth-text").removeClass('active');
+            $(".tenth-text").removeClass('active');
+            $(".eleventh-text").removeClass('active');
+            $(this).addClass('active');
+
+          });
+
+
+          $(".eightn-text").hover(function(){
+            $(".eightn-image").fadeIn();
+            $(".first-image").fadeOut();
+            $(".second-image").fadeOut();
+            $(".third-image").fadeOut();
+            $(".fourth-image").fadeOut();
+            $(".fifth-image").fadeOut();
+            $(".sixth-image").fadeOut();
+            $(".seventh-image").fadeOut();
+            $(".ninth-image").fadeOut();
+            $(".tenth-image").fadeOut();
+            $(".eleventh-image").fadeOut();
+            $(".first-text").removeClass('active');
+            $(".second-text").removeClass('active');
+            $(".third-text").removeClass('active');
+            $(".fourth-text").removeClass('active');
+            $(".fifth-text").removeClass('active');
+            $(".sixth-text").removeClass('active');
+            $(".seventh-text").removeClass('active');
+            $(".ninth-text").removeClass('active');
+            $(".tenth-text").removeClass('active');
+            $(".eleventh-text").removeClass('active');
+            $(this).addClass('active');
+
+          });
+
+
+          $(".ninth-text").hover(function(){
+            $(".ninth-image").fadeIn();
+            $(".first-image").fadeOut();
+            $(".second-image").fadeOut();
+            $(".third-image").fadeOut();
+            $(".fourth-image").fadeOut();
+            $(".fifth-image").fadeOut();
+            $(".sixth-image").fadeOut();
+            $(".seventh-image").fadeOut();
+            $(".eightn-image").fadeOut();
+            $(".tenth-image").fadeOut();
+            $(".eleventh-image").fadeOut();
+            $(".first-text").removeClass('active');
+            $(".second-text").removeClass('active');
+            $(".third-text").removeClass('active');
+            $(".fourth-text").removeClass('active');
+            $(".fifth-text").removeClass('active');
+            $(".sixth-text").removeClass('active');
+            $(".seventh-text").removeClass('active');
+            $(".eightn-text").removeClass('active');
+            $(".tenth-text").removeClass('active');
+            $(".eleventh-text").removeClass('active');
+            $(this).addClass('active');
+
+          });
+
+
+          $(".tenth-text").hover(function(){
+            $(".tenth-image").fadeIn();
+            $(".first-image").fadeOut();
+            $(".second-image").fadeOut();
+            $(".third-image").fadeOut();
+            $(".fourth-image").fadeOut();
+            $(".fifth-image").fadeOut();
+            $(".sixth-image").fadeOut();
+            $(".seventh-image").fadeOut();
+            $(".eightn-image").fadeOut();
+            $(".ninth-image").fadeOut();
+            $(".eleventh-image").fadeOut();
+            $(".first-text").removeClass('active');
+            $(".second-text").removeClass('active');
+            $(".third-text").removeClass('active');
+            $(".fourth-text").removeClass('active');
+            $(".fifth-text").removeClass('active');
+            $(".sixth-text").removeClass('active');
+            $(".seventh-text").removeClass('active');
+            $(".eightn-text").removeClass('active');
+            $(".ninth-text").removeClass('active');
+            $(".eleventh-text").removeClass('active');
+            $(this).addClass('active');
+
+          });
+
+          $(".eleventh-text").hover(function(){
+            $(".eleventh-image").fadeIn();
+            $(".first-image").fadeOut();
+            $(".second-image").fadeOut();
+            $(".third-image").fadeOut();
+            $(".fourth-image").fadeOut();
+            $(".fifth-image").fadeOut();
+            $(".sixth-image").fadeOut();
+            $(".seventh-image").fadeOut();
+            $(".eightn-image").fadeOut();
+            $(".ninth-image").fadeOut();
+            $(".tenth-image").fadeOut();
+            $(".first-text").removeClass('active');
+            $(".second-text").removeClass('active');
+            $(".third-text").removeClass('active');
+            $(".fourth-text").removeClass('active');
+            $(".fifth-text").removeClass('active');
+            $(".sixth-text").removeClass('active');
+            $(".seventh-text").removeClass('active');
+            $(".eightn-text").removeClass('active');
+            $(".ninth-text").removeClass('active');
+            $(".tenth-text").removeClass('active');
+            $(this).addClass('active');
+
+          });
+})(jQuery);
